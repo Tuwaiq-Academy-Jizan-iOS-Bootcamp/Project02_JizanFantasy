be79e60382907ccc99c39b3b49fcf3b965a0ecde
Belal <Bealal1995Dohal@gmail.com>
2021-11-04 16:32:37 +0300

    Almost

--- BelalDOHAL_Project02_GameOfTheGames/BelalDOHAL_Project02_GameOfTheGames/BossView.swift
+++ BelalDOHAL_Project02_GameOfTheGames/BelalDOHAL_Project02_GameOfTheGames/BossView.swift
@@ -1,16 +1,16 @@
-//
-//  BossView.swift
-//  BelalDOHAL_Project02_GameOfTheGames
-//
-//  Created by Belal Dohal on 28/03/1443 AH.
-//
-
+struct Boss {
+    var name:String
+    var weaponD:Int
+    var PowerD:Int
+    var specialC:Int
+    var lifeP:Int
+    var defense:Int
+    var specialEffect:String
+}
 import UIKit
 class BossView: UIViewController {
     //Outlet Go Button//
     @IBOutlet weak var challengMeMortal: UIButton!
-    //Images//
-    var uiBossImage = Int.random(in: 1...2)
     //Player Info//
     var playerNL = ""
     var playerImage = 0
@@ -19,10 +19,20 @@ class BossView: UIViewController {
     var powerDValue = 0
     var defenseValue = 0
     var lifePValue = 0
+    //Boss Info//
+    @IBOutlet weak var bossShow: UIImageView!
+    var randomBoss = Int.random(in: 1...2)
+    var boss1 = Boss(name: "Ravana", weaponD: 45, PowerD: 20, specialC: 110, lifeP: 250, defense: 30, specialEffect: "Ravana Ultimate")
+    var boss2 = Boss(name: "Dragoon", weaponD: 30, PowerD: 15, specialC: 75, lifeP: 170, defense: 25, specialEffect: "Dragoon Uitimate")
     //Override//
     override func viewDidLoad() {
         super.viewDidLoad()
         challengMeMortal.layer.cornerRadius = 20
+        if randomBoss == 1 {
+            bossShow.image = UIImage(named: "Ravana")
+        }else {
+            bossShow.image = UIImage(named: "Dragon")
+        }
     }
     //Send Function//
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -34,5 +44,25 @@ class BossView: UIViewController {
         sendInfo?.defenseValue = defenseValue
         sendInfo?.lifePValue = lifePValue
         sendInfo?.playerNL = playerNL
+        //Boss State//
+        if randomBoss == 1 {
+            sendInfo?.bossName = boss1.name
+            sendInfo?.bossImage = randomBoss
+            sendInfo?.bStateLP = boss1.lifeP
+            sendInfo?.bStatePD = boss1.PowerD
+            sendInfo?.bStateSC = boss1.specialC
+            sendInfo?.bStateDEF = boss1.defense
+            sendInfo?.bStateWD = boss1.weaponD
+            sendInfo?.btextSC = boss1.specialEffect
+        }else{
+            sendInfo?.bossName = boss2.name
+            sendInfo?.bossImage = randomBoss
+            sendInfo?.bStateLP = boss2.lifeP
+            sendInfo?.bStatePD = boss2.PowerD
+            sendInfo?.bStateSC = boss2.specialC
+            sendInfo?.bStateWD = boss1.weaponD
+            sendInfo?.bStateDEF = boss2.defense
+            sendInfo?.btextSC = boss2.specialEffect
+        }
     }
 }
--- BelalDOHAL_Project02_GameOfTheGames/BelalDOHAL_Project02_GameOfTheGames/FightView.swift
+++ BelalDOHAL_Project02_GameOfTheGames/BelalDOHAL_Project02_GameOfTheGames/FightView.swift
@@ -1,6 +1,9 @@
 import UIKit
 
 class FightView: UIViewController {
+    @IBOutlet weak var diceLable: UILabel!
+    //----------Player Things//
+    @IBOutlet weak var playerUIImage: UIImageView!
     @IBOutlet weak var playerNameLable: UILabel!
     var playerNL = ""
     var playerImage = 0
@@ -15,17 +18,78 @@ class FightView: UIViewController {
     @IBOutlet weak var lifePLable: UILabel!
     var lifePValue = 0
     @IBOutlet weak var rulleTheDice: UIButton!
-    
+    //----------Boss Things//
+    @IBOutlet weak var bossUIImage: UIImageView!
+    var bossImage = 0
+    @IBOutlet weak var bossLable: UILabel!
+    var bossName = ""
+    @IBOutlet weak var bossLP: UILabel!
+    var bStateLP = 0
+    @IBOutlet weak var bossDEF: UILabel!
+    var bStateDEF = 0
+    @IBOutlet weak var bossPD: UILabel!
+    var bStatePD = 0
+    @IBOutlet weak var bossWD: UILabel!
+    var bStateWD = 0
+    @IBOutlet weak var bossSC: UILabel!
+    var btextSC = ""
+    var bStateSC = 0
+    //Override//
     override func viewDidLoad() {
         super.viewDidLoad()
+        diceLable.text = "Start The Game"
         rulleTheDice.layer.cornerRadius = 20
+        if playerImage == 1 {
+            playerUIImage.image = UIImage(named: "Warrior")
+        }else if playerImage == 2 {
+            playerUIImage.image = UIImage(named: "Mage")
+        }else {
+            playerUIImage.image = UIImage(named: "Rouge")
+        }
         playerNameLable.text = playerNL
         specialCLable.text = "\(specialCValue)"
         weaponDLable.text = "\(weaponDValue)"
         poweDLable.text = "\(powerDValue)"
         defenseLable.text = "\(defenseValue)"
         lifePLable.text = "\(lifePValue)"
+        //Boss//
+        if bossImage == 1 {
+            bossUIImage.image = UIImage(named: "Ravana")
+            bossLable.text = bossName
+            bossLP.text = "\(bStateLP)"
+            bossPD.text = "\(bStatePD)"
+            bossSC.text = btextSC
+            bossWD.text = "\(bStateWD)"
+            bossDEF.text = "\(bStateDEF)"
+        }else {
+            bossUIImage.image = UIImage(named: "Dragon")
+            bossLable.text = bossName
+            bossLP.text = "\(bStateLP)"
+            bossPD.text = "\(bStatePD)"
+            bossSC.text = btextSC
+            bossWD.text = "\(bStateWD)"
+            bossDEF.text = "\(bStateDEF)"
+        }
     }
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    //Play The Game//
+    var turnHow = true
+    @IBAction func rulleTheDice(_ sender: Any) {
+        let rD = Int.random(in: 1...20)
+        if turnHow == true {
+        switch rD {
+        case 1...9: turnHow = false
+        case 10...19: turnHow = false
+        case 20: turnHow = false
+        default: print("ERROR")
+        }
+        }else {
+            switch rD {
+            case 1...9: turnHow = true
+            case 10...19: turnHow = true
+            case 20: turnHow = true
+            default: print("ERROR")
+            }
+        }
     }
 }
+
