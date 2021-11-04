a25d65cc72bc9f55ef566587e96ff0109a5395a6
bushrabarakat1 <Bushra.Barakat.jiz.s@tuwaiq.edu.sa>
2021-11-04 15:09:33 +0300

    bushra Project

--- BushraBarakat_Project02/BushraBarakat_Project02/ViewController.swift
+++ BushraBarakat_Project02/BushraBarakat_Project02/ViewController.swift
@@ -49,7 +49,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var labelHero3: UILabel!
     @IBOutlet weak var labelHero4: UILabel!
     @IBOutlet weak var labelHero5: UILabel!
-    
+    @IBOutlet weak var resultRollingDice: UILabel!
+    @IBOutlet weak var nameBoss: UILabel!
+    @IBOutlet weak var nameHero: UILabel!
+    @IBOutlet weak var heroImage2: UIImageView!
     @IBOutlet weak var bossImage: UIImageView!
     @IBOutlet weak var descripationGame: UILabel!
     
@@ -59,18 +62,25 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
        
         randomBoss()
-        
+       
         
     }
         
     var boss1 = Boss(lifePoint: 250, defense: 30, powerDamage:20, weaponDamage: 45, spcialCapacity: 110)
-
     var boss2 = Boss(lifePoint: 170, defense: 25, powerDamage: 15, weaponDamage: 30, spcialCapacity: 75)
-
+    
+    var knight = Hero(lifePoint: nil, defense: nil, powerDamage: nil, weaponDamage: nil, specialCapacity: nil)
+    var wizerd = Hero(lifePoint: nil, defense: nil, powerDamage: nil, weaponDamage: nil, specialCapacity: nil)
+    var thief = Hero(lifePoint: nil, defense: nil, powerDamage: nil, weaponDamage: nil, specialCapacity: nil)
+    
+    var dead = 0
+    
+    
     func randomBoss(){
     let conect = Int.random(in:1...2)
     switch conect {
     case 1:
+        nameBoss.text = ("Boss1")
         bossImage.image = UIImage(named: "bossImage1")
         label1.text = String (boss1.lifePoint)
         label2.text = String (boss1.defense)
@@ -78,6 +88,7 @@ class ViewController: UIViewController {
         label4.text = String (boss1.weaponDamage)
         label5.text = String (boss1.spcialCapacity)
     case 2:
+        nameBoss.text = ("Boss2")
         bossImage.image = UIImage(named: "bossImage2")
         label1.text = String (boss2.lifePoint)
         label2.text = String (boss2.defense)
@@ -88,40 +99,37 @@ class ViewController: UIViewController {
     default:
        print  ("no number")
     }
-    
     }
-        
+    
+    
+func rollingDice(){
+    let randomDice = Int.random(in:1...20)
+    switch randomDice{
 
-    
-    var turn = 0
-    var winner = false
-    func rollingDice(_ sender: Any) {
-        turn += 1
-        if winner {
-            print("Game Over ! ")
-        } else {
-            print("------- This is the turn N°\(turn)--------")
-            logicOfTheGame()
-            print("============== This is the end of turn N°\(turn)===============")
-        }
-        
-    func logicOfTheGame(){
-        
-        
-            
-            
-            
-            
-            
-        }
-        
-    }
-    
-    @IBAction func unwind(segue: UIStoryboardSegue){
+    case 1...9:
+        resultRollingDice.text = ""
+    case 10...19:
+        resultRollingDice.text = ""
+        print ("weapon Damage")
+    case 20:
+        resultRollingDice.text = ""
+        print ("special Capacity")
+    default:
+        print ("nothing")
     }
 }
 
+   @IBAction func rollingDice(_ sender: Any) {
+      var random = Int.random(in:1...20)
+       resultRollingDice.text = String (random)
+
+       
+   }
 
 
+    
+    
+   @IBAction func unwind(segue: UIStoryboardSegue){
+    }
 
-
+}
--- BushraBarakat_Project02/BushraBarakat_Project02/ViewControllerTwo.swift
+++ BushraBarakat_Project02/BushraBarakat_Project02/ViewControllerTwo.swift
@@ -7,21 +7,37 @@
 
 import Foundation
 import UIKit
+
+struct Heroplayer {
+    var lifePoint : Int?
+    var defense : Int?
+    var powerDamage : Int?
+    var weaponDamage : Int?
+    var specialCapacity : Int?
+    
+}
+
 class ViewControllerTwo: UIViewController{
-    @IBOutlet weak var textFiled5: UITextField!
-    @IBOutlet weak var textFiled4: UITextField!
-    @IBOutlet weak var textFiled3: UITextField!
-    @IBOutlet weak var textFiled2: UITextField!
+  
     @IBOutlet weak var textFiled1: UITextField!
+    @IBOutlet weak var textFiled2: UITextField!
+    @IBOutlet weak var textFiled3: UITextField!
+    @IBOutlet weak var textFiled4: UITextField!
+    @IBOutlet weak var textFiled5: UITextField!
     @IBOutlet weak var newChallenger: UIButton!
     @IBOutlet weak var newChallengeImage: UIImageView!
-    @IBOutlet weak var newChallengName: UILabel!
+    @IBOutlet weak var newChallengerName: UILabel!
+    @IBOutlet weak var point: UILabel!
     
-//pictur array
-    var heroImage = ["kinght", "wizard", "thief"]
-    
+//.............................
+    var knight = Hero(lifePoint: nil, defense: nil, powerDamage: nil, weaponDamage: nil, specialCapacity: nil)
+    var wizerd = Hero(lifePoint: nil, defense: nil, powerDamage: nil, weaponDamage: nil, specialCapacity: nil)
+    var thief = Hero(lifePoint: nil, defense: nil, powerDamage: nil, weaponDamage: nil, specialCapacity: nil)
+ //..............................
+    var theHero = 0
     
     override func viewDidLoad() {
+        super.viewDidLoad()
         //......delegate..........
         textFiled1.delegate = self
         textFiled2.delegate = self
@@ -29,35 +45,47 @@ class ViewControllerTwo: UIViewController{
         textFiled4.delegate = self
         textFiled5.delegate = self
         //.......image............
-        newChallengName.text = ("\(heroImage)")
+       
     }
     
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let text = segue.destination as! ViewController
-        text.labelHero1.text = textFiled1.text
-        text.labelHero2.text = textFiled2.text
-        text.labelHero3.text = textFiled3.text
-        text.labelHero4.text = textFiled4.text
-        text.labelHero5.text = textFiled5.text
+       var sender = segue.destination as! ViewController
+        sender.labelHero1.text = textFiled1.text
+        sender.labelHero2.text = textFiled2.text
+        sender.labelHero3.text = textFiled3.text
+        sender.labelHero4.text = textFiled4.text
+        sender.labelHero5.text = textFiled5.text
     }
     //.........imagefunction..........
     @IBAction func newChallengerButton(_ sender: Any) {
-            
+        theHero += 1
+        switch theHero {
+        case 1 : newChallengeImage.image =  UIImage(named:"knight" )
+            newChallengerName.text = "Knight"
+            point.text = "250"
+        case 2 : newChallengeImage.image = UIImage(named: "wizard")
+            newChallengerName.text = "Wizerd"
+            point.text = "245"
+        case 3 : newChallengeImage.image = UIImage(named: "thief")
+            newChallengerName.text = "Thief"
+            point.text = "210"
+        case 4 :
+            theHero = 0
+        default:
+            print ("no hero")
+        }
         
     }
-    
+
     
 }
-extension ViewControllerTwo: UITextFieldDelegate {
+extension ViewControllerTwo: UITextFieldDelegate{
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
     }
-    
-   
-    
-    
+
 }
 
 
