b85cdf46c77404d3727a15361c18ab8b06d65680
Doaa <doaa.ageel@gmail.com>
2021-11-04 15:48:42 +0300

    project apdate

--- DoaaAlageel_project ٣/DoaaAlageel_project/ViewController.swift
+++ DoaaAlageel_project ٣/DoaaAlageel_project/ViewController.swift
@@ -27,11 +27,8 @@ class User:rollGameUser {
     var weapondamage: Int?
     var specialeffect: Int
 
-    var dFMax:Int
-    var pDMax:Int
-    var wDMax:Int
-    
-    init(name: String,points:Int,lifepoint:Int?,defend:Int?,powerdamage:Int?,weapondamage:Int?,specialeffect:Int,dFMax:Int,pDMax:Int,wDMax:Int){
+   
+    init(name: String,points:Int,lifepoint:Int?,defend:Int?,powerdamage:Int?,weapondamage:Int?,specialeffect:Int){
         self.name=name
         self.points=points
         self.lifepoint=lifepoint
@@ -40,36 +37,68 @@ class User:rollGameUser {
         self.weapondamage=weapondamage
         self.specialeffect=specialeffect
        
-       self.dFMax=dFMax
-       self.pDMax=pDMax
-       self.wDMax=wDMax
+       
     }
+}
 
 class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var dscripGame: UITextView!
     
     
     @IBOutlet weak var lableUserName: UILabel!
-    
     @IBOutlet weak var lableLP: UILabel!
-    
     @IBOutlet weak var lableDF: UILabel!
-    
     @IBOutlet weak var lablePD: UILabel!
-    
     @IBOutlet weak var lableWD: UILabel!
-    
     @IBOutlet weak var lableSC: UILabel!
     
     
+    @IBOutlet weak var labelUsername2: UILabel!
+    @IBOutlet weak var user2LP: UILabel!
     
+    @IBOutlet weak var user2DF: UILabel!
+    @IBOutlet weak var user2PD: UILabel!
+    @IBOutlet weak var user2WD: UILabel!
+    @IBOutlet weak var user2SC: UILabel!
     
-    
-    
-    
+    var user1 = User(name: "user1", points: 0, lifepoint: 250, defend: 30, powerdamage: 20, weapondamage: 45, specialeffect: 110)
+    var user2 = User(name: "user2", points: 0, lifepoint: 170, defend: 25, powerdamage: 15, weapondamage: 30, specialeffect: 75)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let userS = Int.random(in:1...2)
+        switch userS{
+            
+        case 1 :
+            
+    lableUserName.text = user1.name
+    lableLP.text = String(user1.lifepoint!)
+            lableDF.text = String(user1.defend!)
+            lablePD.text = String(user1.powerdamage!)
+            lableWD.text = String(user1.weapondamage!)
+            lableSC.text = String(user1.specialeffect)
+            
+            
+        case 2 :
+            
+            lableUserName.text = user2.name
+            lableLP.text = String(user2.lifepoint!)
+            lableDF.text = String(user2.defend!)
+            lablePD.text = String(user2.powerdamage!)
+            lableWD.text = String(user2.weapondamage!)
+            lableSC.text = String(user2.specialeffect)
+            
+        
+            
+        default:
+            print ("error")
+        
+            
+        }
+        
+        
     }
 
     @IBAction func buttonPlus(_ sender: Any) {
@@ -79,9 +108,85 @@ class ViewController: UIViewController {
         print("to Root View Controller")
     }
   
+   
+    @IBAction func rolligdice(_ sender: UIButton) {
+        var lifp1 = Int(lableLP.text!)!
+        var def1 = Int(lableDF.text!)!
+        var pow1 = Int(lablePD.text!)!
+        var weap1 = Int(lableWD.text!)!
+      var spec1 = Int(lableSC.text!)!
+   
+    var lifp2 = Int(user2LP.text!)!
+        var def2 = Int(user2DF.text!)!
+        var pow2 = Int(user2PD.text!)!
+        var weap2 = Int(user2WD.text!)!
+        var spec2 = Int(user2SC.text!)!
+        
+        let rolradaic = Int.random(in:1...20)
+        switch rolradaic {
+        case 1...9 :
+            if ((def2 - pow2) <= 0){
+                lifp2 = (lifp2 + (def2 - pow1 ))
+                } else {
+                    lifp2 = (lifp2 - 0)
+                }
+            lableLP.text = String(lifp2)
+            
+        print("user use dp")
+        
+    case 10...19 :
+            if ((def2 - weap1) <= 0){
+        lifp2 = (lifp2 + (def2 - weap1))
+                 }
+                 else{
+            lifp2 = (lifp2 - 0)
+        }
+            lableLP.text = String(lifp2)
+            
+        default:
+            print ("error")
+    }
     
-
+        switch rolradaic {
+        case 1...9 :
+            if ((def1 - pow1) <= 0){
+                lifp1 = (lifp1 + (def1 - pow2 ))
+                } else {
+                    lifp1 = (lifp1 - 0)
+                }
+            lableLP.text = String(lifp2)
+            
+        print("user use dp")
+        
+    case 10...19 :
+            if ((def1 - weap2) <= 0){
+        lifp1 = (lifp1 + (def1 - weap2))
+                 }
+                 else{
+            lifp1 = (lifp1 - 0)
+        }
+            user2LP.text = String(lifp1)
+            
+            
+            
+            
+        
+        default:
+            print ("error")
+        
+    
+    }
+        
+       
+            
+        }
+        
+        
+        
+        
+        
 }
 
-}
+
+
 
--- DoaaAlageel_project ٣/DoaaAlageel_project/ViewControllerTwo.swift
+++ DoaaAlageel_project ٣/DoaaAlageel_project/ViewControllerTwo.swift
@@ -45,10 +45,53 @@ class Herro:rollGameHero {
        self.pDMax=pDMax
        self.wDMax=wDMax
     }
+}
 
+class Knight:Herro{
+   
+    
+}
 
+class Wizard:Herro{
+   
 
-    class ViewControllerTwo: UIViewController, UITextFieldDelegate {
+    
+}
+
+class Thief:Herro{
+   
+    
+}
+
+class ViewControllerTwo: UIViewController{
+    
+    
+    @IBOutlet weak var viewamage: UIImageView!
+    
+@IBOutlet weak var Namepler2lable: UILabel!
+        
+@IBOutlet weak var nameplyer: UITextField!
+        
+@IBOutlet weak var pointss: UILabel!
+        
+@IBOutlet weak var lifepoint1: UITextField!
+        
+@IBOutlet weak var defend1: UITextField!
+        
+@IBOutlet weak var powerdamage1: UITextField!
+        
+    @IBOutlet weak var weapondamage1: UITextField!
+    
+    @IBOutlet weak var SClable1: UILabel!
+    
+    var knight = Knight(name:"Knight", points: 250, lifepoint: nil, defend:nil, powerdamage:nil, weapondamage:nil, specialeffect:100,dFMax:20, pDMax:30, wDMax:40)
+    
+    var wizard = Wizard(name:"Wizard", points: 245, lifepoint: nil, defend:nil, powerdamage:nil, weapondamage:nil, specialeffect:50, dFMax:15, pDMax:70, wDMax:20)
+        
+    var thief = Thief(name:"Thief", points: 210,  lifepoint: nil, defend:nil, powerdamage:nil, weapondamage:nil, specialeffect:75,dFMax:25, pDMax:15, wDMax:30)
+    
+        var amage = 0
+        
 
     @IBOutlet var TextFName: [UITextField]!
     
@@ -56,20 +99,88 @@ class Herro:rollGameHero {
         super.viewDidLoad()
         for i in TextFName{
             i.delegate = self
+            
         }
+    
+        
        
-    }
+        let player2 = nameplyer.text
+        
+        switch player2 {
+        case "knight":
+    lifepoint1.text = String(knight.lifepoint ?? 60)
+    defend1.text = String(knight.defend ?? 20)
+            
+powerdamage1.text = String(knight.powerdamage ?? 30)
+weapondamage1.text = String(knight.weapondamage ?? 40)
+            
+    case "wizard":
+            lifepoint1.text = String(wizard.lifepoint ?? 90)
+            defend1.text = String(wizard.defend ?? 15)
+            powerdamage1.text = String(wizard.powerdamage ?? 70)
+            weapondamage1.text = String(wizard.weapondamage ?? 20)
+        
+        
+        case "thief" :
+            lifepoint1.text = String(thief.lifepoint ?? 65)
+            defend1.text = String(thief.defend ?? 25)
+            powerdamage1.text = String(thief.powerdamage ?? 15)
+            weapondamage1.text = String(thief.weapondamage ?? 30)
+        default :
+            print(" enter the name of player 2 ")
+
+        
+        
     }
    
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let rootVC = segue.destination as! ViewController
+    rootVC.labelUsername2.text = lifepoint1.text
+        rootVC.labelUsername2.text = nameplyer.text
+    rootVC.user2LP.text = lifepoint1.text
+        rootVC.user2DF.text = defend1.text
+        rootVC.user2PD.text = powerdamage1.text
+        rootVC.user2WD.text = weapondamage1.text
+        rootVC.user2SC.text = SClable1.text
+        
+        
+    }
+    
+    @IBAction func buttonamage(_ sender: Any) {
+       
+            
+        }
+        
+        
+        
+    }
+    
 
         extension ViewControllerTwo:UITextFieldDelegate{
             func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+                
+                if nameplyer.text == "knight" {
+                    pointss.text = String(knight.points)
+                    SClable1.text = String(knight.specialeffect)
+                }
+                else if nameplyer.text == "wizard" {
+                    pointss.text = String(wizard.points)
+                    SClable1.text = String(wizard.specialeffect)
+                }
+                else if nameplyer.text == "thief" {
+                    pointss.text = String(thief.points)
+                    SClable1.text = String(thief.specialeffect)
+                }
+        textField.resignFirstResponder()
                 return true
                 
             }
         }
-            
-}
+    
+
             
             
             
