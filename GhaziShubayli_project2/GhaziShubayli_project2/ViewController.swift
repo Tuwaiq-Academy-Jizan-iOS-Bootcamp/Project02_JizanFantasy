2dfaab048d58bfcc50eeb44446586052f4a36a77
GhaziShubayli <ghazi.shubayli.jiz.s@tuwaiq.edu.sa>
2021-11-04 09:11:43 +0300

    first commit for project files

---
+++ GhaziShubayli_project2/GhaziShubayli_project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GhaziShubayli_project2
+//
+//  Created by ماك بوك on 29/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
