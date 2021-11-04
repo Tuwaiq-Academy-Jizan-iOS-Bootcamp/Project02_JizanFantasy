39664ae290d317f3b99e45cf748c7378d5c039df
Ehab hakami <ehab7k@gmail.com>
2021-11-04 14:36:03 +0300

    updateRandomBoss

--- ehabHakami_jazanFan/ehabHakami_jazanFan/ViewController.swift
+++ ehabHakami_jazanFan/ehabHakami_jazanFan/ViewController.swift
@@ -34,11 +34,18 @@ class Hero {
     func specialCapacit00() {
         lifePoint += specialCapacity
         print("The Hero \(name) has healed \(specialCapacity) point of life\(lifePoint + specialCapacity) and wapen \(weaponDamage + specialCapacity)")
+    }
+        func attackWithPower(){
+            print("The Hero use \(name) power and inflict \(powerDamage + defense)")
+        }
+        func attackWithWeapon() {
+            print("The Hero use \(name) weapon and inflict \(weaponDamage + defense)")
+        }
         
         
     }
 
-}
+
 class Knight: Hero {
 
     func restOfLife() {
@@ -47,13 +54,7 @@ class Knight: Hero {
         }
         print("Rest of life point for Knight : \(lifePoint)")
     }
-    func attackWithPower(){
-        print("The Hero use \(name) power and inflict \(powerDamage + defense)")
-    }
-    func attackWithWeapon() {
-        print("The Hero use \(name) weapon and inflict \(weaponDamage + defense)")
-    }
-    
+
 }
 class Boss2 : Hero {
     func restOfLife() {
@@ -61,12 +62,6 @@ class Boss2 : Hero {
             lifePoint = 0
         }
         print("Rest of life for Boss 1 : \(lifePoint)")
-    }
-    func attackWithPower(){
-        print("The Hero use \(name) power and inflict \(powerDamage + defense)")
-    }
-    func attackWithWeapon() {
-        print("The Hero use \(name) weapon and inflict \(weaponDamage + defense)")
     }
     
 
@@ -81,13 +76,26 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var numDice: UILabel!
     
-        
+    
+    @IBOutlet weak var labelDfValue: UILabel!
+    
+    @IBOutlet weak var labelPdValue: UILabel!
+    
+    @IBOutlet weak var labelWdValue: UILabel!
+    
+    @IBOutlet weak var labelsCValue: UILabel!
+    
+    var player1:Hero?
+    var player2:Hero?
+    
+    
     var turn: Int = 0
     var winner = false
     
-    var kinght = Knight(name: "kinght", lifePoint: 60, defense: 20, powerDamage: 30, weaponDamage: 40, specialCapacity: 100)
+    var kinght = Hero(name: "kinght", lifePoint: 60, defense: 20, powerDamage: 30, weaponDamage: 40, specialCapacity: 100)
     
-    var boss2 = Boss2(name: "boss2", lifePoint: 170, defense: 25, powerDamage: 15, weaponDamage: 30, specialCapacity: 75)
+    var boss2 = Hero(name: "boss2", lifePoint: 170, defense: 25, powerDamage: 15, weaponDamage: 30, specialCapacity: 75)
+    var boss1 = Hero(name: "boss1", lifePoint: 240, defense: 30, powerDamage: 20, weaponDamage: 45, specialCapacity: 110)
 
 
 
@@ -97,11 +105,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let player = Int.random(in: 1...2)
+        if player == 1 {
+            player1 = boss1
+        }else{
+            player1 = boss2
+        }
 
 
 
 
     }
+    
 
     @IBAction func rollingDice(_ sender: UIButton) {
         
@@ -123,31 +138,31 @@ class ViewController: UIViewController {
         switch rollingDice() {
          case 1...4:
             kinght.attackWithPower()
-            boss2.lifePoint -= kinght.powerDamage
-             boss2.lifePoint += boss2.defense
-           viewPrintFight.text += ("kinght used powerDamage : \(kinght.powerDamage)")
-            viewPrintFight.text += ("Boss2 Live Point : \(boss2.lifePoint)")
+            player1!.lifePoint -= kinght.powerDamage
+            player1!.lifePoint += player1!.defense
+            viewPrintFight.text += ("\(kinght.name) used powerDamage ")
+            viewPrintFight.text += ("\(String(describing: player1!.name)) Live Point : \(String(describing: player1!.lifePoint))")
         case 5...9:
            
-             boss2.attackWithPower()
-             kinght.lifePoint -= boss2.powerDamage
+            player1!.attackWithPower()
+             kinght.lifePoint -= player1!.powerDamage
              kinght.lifePoint += kinght.defense
-            viewPrintFight.text += ("Boss2 used powerDamage : \(boss2.powerDamage)")
+            viewPrintFight.text += ("\(player1!.name) used powerDamage")
             viewPrintFight.text += ("kinght Live Point : \(kinght.lifePoint)")
            
          case 10...14:
             
             kinght.attackWithPower()
-             boss2.lifePoint -= kinght.weaponDamage
-             boss2.lifePoint += boss2.defense
+             player1!.lifePoint -= kinght.weaponDamage
+             player1!.lifePoint += player1!.defense
             viewPrintFight.text += ("kinght used weaponDamage : \(kinght.weaponDamage)")
-            viewPrintFight.text += ("Boss2 Live Point : \(boss2.lifePoint)")
+            viewPrintFight.text += ("\(player1!.name) Live Point : \(String(describing: player1!.lifePoint))")
            
         case 15...19:
-            boss2.attackWithWeapon()
-             kinght.lifePoint -= boss2.weaponDamage
+            player1!.attackWithWeapon()
+             kinght.lifePoint -= player1!.weaponDamage
              kinght.lifePoint += kinght.defense
-            viewPrintFight.text += ("Boss2 used weaponDamage : \(boss2.weaponDamage)")
+            viewPrintFight.text += ("\(player1!.name) used weaponDamage : \(String(describing: player1!.weaponDamage))")
             viewPrintFight.text += ("kinght Live Point : \(kinght.lifePoint)")
            
            
@@ -157,20 +172,20 @@ class ViewController: UIViewController {
            kinght.weaponDamage += 10
            viewPrintFight.text += ("the lifepoint of my hero : \(kinght.lifePoint)")
             ///////////////////000000///////////////////////////////////
-            boss2.specialCapacit00()
-           boss2.lifePoint += 5
-           boss2.powerDamage += 32
-           viewPrintFight.text += ("the lifepoint of boss2 : \(boss2.lifePoint)")
+            player1!.specialCapacit00()
+           player1!.lifePoint += 5
+           player1!.powerDamage += 32
+            viewPrintFight.text += ("the lifepoint of \(String(describing: player1!.name)) : \(String(describing: player1!.lifePoint))")
            
-           print ("get \(boss2.lifePoint) , \(boss2.powerDamage)")
+            print ("get \(String(describing: player1!.lifePoint)) , \(String(describing: player1!.powerDamage))")
          default:
            print("Error")
         }
-        theWinnerIs(kinght: kinght, boss2: boss2)
+        theWinnerIs(kinght: kinght, player: player1!)
 
         labelVa.text = "\(kinght.lifePoint)"
         
-        BossLiveP.text = "\(boss2.lifePoint)"
+        BossLiveP.text = "\(String(describing: player1!.lifePoint))"
         
         
             
@@ -178,14 +193,14 @@ class ViewController: UIViewController {
 
     }
 
-    func theWinnerIs(kinght: Hero, boss2: Hero) {
+    func theWinnerIs(kinght: Hero, player: Hero) {
 
         if (kinght.lifePoint <= 0) {
-            print("The Winner is : \(boss2.name) at turn \(turn)")
+            print("The Winner is : \(String(describing: player1!.name)) at turn \(turn)")
             winner = true
         }
-        if (boss2.lifePoint <= 0) {
-            print("The winner is : \(kinght.name) at turn \(turn)")
+        if (player1!.lifePoint <= 0) {
+            print("The winner is :\(kinght.name) at turn \(turn)")
             winner = true
         }
     }
@@ -200,9 +215,31 @@ class ViewController: UIViewController {
         return dice
     }
     
+    
+    @IBAction func nextView(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func goToFight(segue: UIStoryboardSegue){
+        
+    }
+    
+    
+    
+    
+    
+    
 
 
 
 
+}
+
+extension ViewControllerTwo:UITextFieldDelegate{
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
--- ehabHakami_jazanFan/ehabHakami_jazanFan/ViewControllerTwo.swift
+++ ehabHakami_jazanFan/ehabHakami_jazanFan/ViewControllerTwo.swift
@@ -9,24 +9,53 @@ import UIKit
 
 class ViewControllerTwo: UIViewController {
     
+    @IBOutlet weak var textFieldLp: UITextField!
+   
+    
+    @IBOutlet weak var textFieldDf: UITextField!
     
     
-
+    @IBOutlet weak var textFieldPd: UITextField!
+    
+    
+    @IBOutlet weak var textFieldLpWd: UITextField!
+    
+    @IBOutlet weak var cS: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        textFieldLp.delegate = self
+        textFieldDf.delegate = self
+        textFieldPd.delegate = self
+        textFieldLpWd.delegate = self
+        cS.delegate = self
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
+        let rootVC = segue.destination as! ViewController
+        
+        
+        
+        rootVC.labelDfValue.text = textFieldDf.text
+        
+        rootVC.labelVa.text = textFieldLp.text
+        rootVC.labelPdValue.text = textFieldPd.text
+        
+        rootVC.labelWdValue.text = textFieldLpWd.text
+        
+        rootVC.labelsCValue.text = cS.text
+        
         // Pass the selected object to the new view controller.
     }
-    */
+    
 
 }
