ba34ca9d717e38d2e886c347639d779052f807b8
hamad <7amad7arisi@gmail.com>
2021-11-06 20:11:54 +0300

    MP02.3

--- Project02.1_JizanFantasy/Project02.1_JizanFantasy/ViewController.swift
+++ Project02.1_JizanFantasy/Project02.1_JizanFantasy/ViewController.swift
@@ -68,19 +68,23 @@ class Knight:Player2
         super.init(name: name, lifePoint: lifePoint, defense: defense, powerDamage: powerDamage, Weapondamage: Weapondamage, Specialcapacity: Specialcapacity)
     }
 }
-
 class ViewController: UIViewController
 {
-    
     var index: Int = 0
     var myImages2 = ["boss1","boss2"]
+  //  var heroImageinVC1 = 0
+   // var index: Int = 0
+
+    var myImages = ["Wizard","Thief","Knight"]
     
+    @IBOutlet var vsLabel: UILabel!
     @IBOutlet var lpHeroLabel: UILabel!
     @IBOutlet var dfHeroLabel: UILabel!
     @IBOutlet var pdHeroLabel: UILabel!
     @IBOutlet var wdHeroLabel: UILabel!
     @IBOutlet var scHeroLabel: UILabel!
     
+    @IBOutlet var descrptionTextView: UITextView!
     
     @IBOutlet var heroImageinVC1: UIImageView!
     @IBOutlet var bossImage: UIImageView!
@@ -90,63 +94,18 @@ class ViewController: UIViewController
     {
     super.viewDidLoad()
         
-       
+      
+    bossImage.image = UIImage(named: myImages2[Int.random(in: 0...1)])
         
-        bossImage.image = UIImage(named: myImages2[Int.random(in: 0...1)])
+        
         bossSwitch()
-        // print("\(String(describing: bossImage))")
-//
-//        switch bossImage.image
-//        {
-//        case UIImage(named: myImages2[0]):
-//            bossLP1.text = "\(boos1.lifePoint)"
-//            bossDF1.text = "\(boos1.defense)"
-//            bossPD1.text = "\(boos1.powerDamage)"
-//            bossWD1.text = "\(boos1.Weapondamage)"
-//            bossSC1.text = "\(boos1.Specialcapacity)"
-//        case UIImage(named: myImages2[1]):
-//            bossLP1.text = "\(boos2.lifePoint)"
-//            bossDF1.text = "\(boos2.defense)"
-//            bossPD1.text = "\(boos2.powerDamage)"
-//            bossWD1.text = "\(boos2.Weapondamage)"
-//            bossSC1.text = "\(boos2.Specialcapacity)"
-//         default:
-//         print("ERORR ?!$##%#$%^@$^@")
-//        }
-
-    
-//        bossLP1.text = "\(boos1.lifePoint)"
-//        bossLP1.text = "\(boos2.lifePoint)"
-//    switch bossImage
-//    {
-//    case :
-//    bossLP1.text = "\(boos1.lifePoint)"
-//    bossDF1.text = "\(boos1.defense)"
-//    bossPD1.text = "\(boos1.powerDamage)"
-//    bossWD1.text = "\(boos1.Weapondamage)"
-//    bossSC1.text = "\(boos1.Specialcapacity)"
-//    case :
-//    bossLP1.text = "\(boos2.lifePoint)"
-//    bossDF1.text = "\(boos2.defense)"
-//    bossPD1.text = "\(boos2.powerDamage)"
-//    bossWD1.text = "\(boos2.Weapondamage)"
-//    bossSC1.text = "\(boos2.Specialcapacity)"
-//    default:
-//    print("ERORR ?!$##%#$%^@$^@")
-//    }
+      //  heroSwitch()
         
-//        if myImages2[index] == myImages2[0]
-//        {
-//        let boss1LP1 = 250
-//        bossLP1.text = "\(boss1LP1)"
-//        let boss1DF1 = 30
-//        bossDF1.text = "\(boss1DF1)"
-//        }else{
-//        let boss1LP2 = 170
-//        bossLP1.text = "\(boss1LP2)"
-//        let boss1DF2 = 25
-//        bossDF1.text = "\(boss1DF2)"
-//        }
+        
+        
+        // description
+//        let myNumber = ""
+        descrptionTextView.text = "\(description)"
 
     }
     
@@ -158,97 +117,43 @@ class ViewController: UIViewController
     @IBOutlet var bossSC1: UILabel!
     
     
+    
+    func description()
+    {
+        boos2.defense += boos2.lifePoint
+    }
+    
+    var turnNumber = 0
+    
     var boos1 = Boss1(name: "boss1", lifePoint: 250, defense: 30, powerDamage: 20, Weapondamage: 45, Specialcapacity: 110)
     var boos2 = Boss2(name: "boss2", lifePoint: 170, defense: 25, powerDamage: 15, Weapondamage: 30, Specialcapacity: 75)
+    var hero = Player1(name: "", lifePoint: 0, defense: 0, powerDamage: 0, Weapondamage: 0, Specialcapacity: 0 )
     
-//    var knight = Knight.init(name: "Knight", lifePoint: 60, defense: 20, powerDamage: 30, Weapondamage: 40, Specialcapacity: 100)
-//    var wizard = Wizard.init(name: "Wizard", lifePoint: 90, defense: 15, powerDamage: 70, Weapondamage: 20, Specialcapacity: 50)
-//    var thief = Thief.init(name: "Thief", lifePoint: 65, defense: 25, powerDamage: 15, Weapondamage: 30, Specialcapacity: 70)
+   //var dfHeroLabel.text = "250"
+  //  var dfHero = dfHeroLabel.text
+    
+//    var wizard = Wizard(name: "Wizard", lifePoint: 90, defense: 15, powerDamage: 70, Weapondamage: 20, Specialcapacity: 50)
+//    var knight = Knight(name: "Knight", lifePoint: 60, defense: 20, powerDamage: 30, Weapondamage: 40, Specialcapacity: 100)
+//    var thief = Thief(name: "Thief", lifePoint: 65, defense: 25, powerDamage: 15, Weapondamage: 30, Specialcapacity: 70)
+//    var boss1power = Power(name: "gggggggg", damage: 35, attackAction: "x", specialEffect: 55)
 //
-
-//
+//    func power()
 //    {
-//    case ["boss1"]:
-//    bossLP1.text = "\(boos1.lifePoint)"
-//    bossDF1.text = "\(boos1.defense)"
-//    bossPD1.text = "\(boos1.powerDamage)"
-//    bossWD1.text = "\(boos1.Weapondamage)"
-//    bossSC1.text = "\(boos1.Specialcapacity)"
-//    case ["boss2"]:
-//    bossLP1.text = "\(boos2.lifePoint)"
-//    bossDF1.text = "\(boos2.defense)"
-//    bossPD1.text = "\(boos2.powerDamage)"
-//    bossWD1.text = "\(boos2.Weapondamage)"
-//    bossSC1.text = "\(boos2.Specialcapacity)"
-//    default:
-//    print("ERORR ?!$##%#$%^@$^@")
+//        vsLabel.text = "\(boss1power.attackAction)"
 //    }
-//    func boss()
-//    {
-//        for _ in myImages2
-//        {
-//        switch myImages2
-//        {
-//        case ["boss1"]:
-//        let boss1LP1 = 250
-//                bossLP1.text = "\(boss1LP1)"
-//        let boss1DF1 = 30
-//                bossDF1.text = "\(boss1DF1)"
-//        break
-//        case ["boss2"]:
-//        let boss1LP1 = 170
-//                    bossLP1.text = "\(boss1LP1)"
-//        let boss1DF1 = 30
-//                    bossDF1.text = "\(boss1DF1)"
-//        break
-//        default:
-//            print("ERORR ?!$##%#$%^@$^@")
-//        }
-//        }
-
-//func boss()
+//  func boss1()
 //    {
 //    if myImages2[index] == myImages2[0]
-//   {
-//
-//    else if myImages2[index] == myImages2[1]
-//    {
-//    let boss1LP2 = 170
-//    bossLP1.text = "\(boss1LP2)"
-//    let boss1DF2 = 25
-//    bossDF1.text = "\(boss1DF2)"
-//    print("1241235462562")
-//
+//        {
+//        bossLP1.text = "\(boos1.lifePoint)"
+//        bossDF1.text = "\(boos1.defense)"
 //    }
+//     else if myImages2[index] == myImages2[1]
+//     {
+//    bossLP1.text = "\(boos2.lifePoint)"
+//    bossDF1.text = "\(boos2.defense)"
+//     }
 //    }
-//    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-  func boss1()
-    {
-    if myImages2[index] == myImages2[0]
-        {
-        bossLP1.text = "\(boos1.lifePoint)"
-        bossDF1.text = "\(boos1.defense)"
-    }
-     else if myImages2[index] == myImages2[1]
-     {
-    bossLP1.text = "\(boos2.lifePoint)"
-    bossDF1.text = "\(boos2.defense)"
-     }
-    }
     func bossSwitch()
     {
     switch bossImage.image
@@ -270,27 +175,303 @@ class ViewController: UIViewController
     }
     }
     
-    
-    
-    
+//    func heroSwitch()
+//    {
+//    switch heroImageinVC1.image
+//    {
+//    case UIImage(named: myImages[0]):
+//        lpHeroLabel.text = "\(wizard.lifePoint)"
+//        dfHeroLabel.text = "\(wizard.defense)"
+//        pdHeroLabel.text = "\(wizard.powerDamage)"
+//        wdHeroLabel.text = "\(wizard.Weapondamage)"
+//        scHeroLabel.text = "\(wizard.Specialcapacity)"
+//    case UIImage(named: myImages[1]):
+//        lpHeroLabel.text = "\(knight.lifePoint)"
+//        dfHeroLabel.text = "\(knight.defense)"
+//        pdHeroLabel.text = "\(knight.powerDamage)"
+//        wdHeroLabel.text = "\(knight.Weapondamage)"
+//        scHeroLabel.text = "\(knight.Specialcapacity)"
+//    case UIImage(named: myImages[2]):
+//        lpHeroLabel.text = "\(thief.lifePoint)"
+//        dfHeroLabel.text = "\(thief.defense)"
+//        pdHeroLabel.text = "\(thief.powerDamage)"
+//        wdHeroLabel.text = "\(thief.Weapondamage)"
+//        scHeroLabel.text = "\(thief.Specialcapacity)"
+//     default:
+//     print("ERORR ?!$##%#$%^@$^@")
+//    }
+//    }
     
     
     
     @IBAction func rollDiceButton(_ sender: UIButton)
     {
-       // boss1()
         
+        
+       // description()
+        
+        
+        
+               
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+     //   boss1()
+        
+turnNumber += 1
+print("Turn Number  \(turnNumber)")
+
+diceLabel.text = "Dice = \(rollingDice())"
+func rollingDice() -> Int
+{
     
-        diceLabel.text = "Dice = \(rollingDice())"
-        func rollingDice() -> Int
-        {
-            var dice: Int
-            dice = Int.random(in: 1...20)
-            print("Dice = \(dice)")
+var dice: Int
+dice = Int.random(in: 1...20)
+print("Dice = \(dice)")
+        
             
-            return dice
-        }
-    }
+if dice == 1
+{
+switch bossImage.image
+{
+case UIImage(named: myImages2[0]):
+print("\(boos1.lifePoint)")
+print("\(boos2.lifePoint)")
+//boos2.Weapondamage
+boos2.lifePoint -= boos1.Weapondamage
+case UIImage(named: myImages2[1]):
+print("\(boos1.lifePoint)")
+print("\(boos2.lifePoint)")
+boos2.lifePoint -= boos1.Weapondamage
+default:
+print("Error")
+}
+//                boos2.Weapondamage
+//                boos2.lifePoint += boos1.Weapondamage
+}
+else if dice == 2
+{
+switch bossImage.image
+{
+case UIImage(named: myImages2[0]):
+print("\(boos1.lifePoint)")
+case UIImage(named: myImages2[1]):
+print("\(boos2.lifePoint)")
+default:
+print("Error")
+}
+}
+else if dice == 3
+{
+switch bossImage.image
+{
+case UIImage(named: myImages2[0]):
+print("\(boos1.lifePoint)")
+case UIImage(named: myImages2[1]):
+print("\(boos2.lifePoint)")
+default: print("Error")
+}
+}
+else if dice == 4
+{
+switch bossImage.image
+{
+case UIImage(named: myImages2[0]):
+print("\(boos1.lifePoint)")
+case UIImage(named: myImages2[1]):
+print("\(boos2.lifePoint)")
+default: print("Error")
+}
+}
+else if dice == 5
+{
+switch bossImage.image
+{
+case UIImage(named: myImages2[0]):
+print("\(boos1.lifePoint)")
+case UIImage(named: myImages2[1]):
+print("\(boos2.lifePoint)")
+default: print("Error")
+}
+}
+else if dice == 6
+{
+switch bossImage.image
+{
+case UIImage(named: myImages2[0]):
+print("\(boos1.lifePoint)")
+case UIImage(named: myImages2[1]):
+print("\(boos2.lifePoint)")
+default: print("Error")
+}
+}
+else if dice == 7
+{
+switch bossImage.image
+{
+case UIImage(named: myImages2[0]):
+print("\(boos1.lifePoint)")
+case UIImage(named: myImages2[1]):
+print("\(boos2.lifePoint)")
+default: print("Error")
+}
+}
+else if dice == 8
+{
+switch bossImage.image
+{
+case UIImage(named: myImages2[0]):
+print("\(boos1.lifePoint)")
+case UIImage(named: myImages2[1]):
+print("\(boos2.lifePoint)")
+default: print("Error")
+}
+}
+else if dice == 9
+{
+switch bossImage.image
+{
+case UIImage(named: myImages2[0]):
+print("\(boos1.lifePoint)")
+case UIImage(named: myImages2[1]):
+print("\(boos2.lifePoint)")
+default: print("Error")
+}
+}
+else if dice == 10
+{
+switch bossImage.image
+{
+case UIImage(named: myImages2[0]):
+print("\(boos1.lifePoint)")
+case UIImage(named: myImages2[1]):
+print("\(boos2.lifePoint)")
+default: print("Error")
+}
+}
+else if dice == 11
+{
+switch bossImage.image
+{
+case UIImage(named: myImages2[0]):
+print("\(boos1.lifePoint)")
+case UIImage(named: myImages2[1]):
+print("\(boos2.lifePoint)")
+default: print("Error")
+}
+}
+else if dice == 12
+{
+switch bossImage.image
+{
+case UIImage(named: myImages2[0]):
+print("\(boos1.lifePoint)")
+case UIImage(named: myImages2[1]):
+print("\(boos2.lifePoint)")
+default:print("Error")
+}
+}
+else if dice == 13
+{
+switch bossImage.image
+{
+case UIImage(named: myImages2[0]):
+print("\(boos1.lifePoint)")
+case UIImage(named: myImages2[1]):
+print("\(boos2.lifePoint)")
+default: print("Error")
+}
+}
+else if dice == 14
+{
+switch bossImage.image
+{
+case UIImage(named: myImages2[0]):
+print("\(boos1.lifePoint)")
+case UIImage(named: myImages2[1]):
+print("\(boos2.lifePoint)")
+default:
+print("Error")
+}
+}
+else if dice == 15
+{
+switch bossImage.image
+{
+case UIImage(named: myImages2[0]):
+print("\(boos1.lifePoint)")
+case UIImage(named: myImages2[1]):
+print("\(boos2.lifePoint)")
+default: print("Error")
+}
+}
+else if dice == 16
+{
+switch bossImage.image
+{
+case UIImage(named: myImages2[0]):
+print("\(boos1.lifePoint)")
+case UIImage(named: myImages2[1]):
+print("\(boos2.lifePoint)")
+default: print("Error")
+}
+}
+else if dice == 17
+{
+switch bossImage.image
+{
+case UIImage(named: myImages2[0]):
+print("\(boos1.lifePoint)")
+case UIImage(named: myImages2[1]):
+print("\(boos2.lifePoint)")
+default: print("Error")
+}
+}
+else if dice == 18
+{
+switch bossImage.image
+{
+case UIImage(named: myImages2[0]):
+print("\(boos1.lifePoint)")
+case UIImage(named: myImages2[1]):
+print("\(boos2.lifePoint)")
+default: print("Error")
+}
+}
+else if dice == 19
+{
+switch bossImage.image
+{
+case UIImage(named: myImages2[0]):
+print("\(boos1.lifePoint)")
+case UIImage(named: myImages2[1]):
+print("\(boos2.lifePoint)")
+default: print("Error")
+}
+}
+else if dice == 20
+{
+switch bossImage.image
+{
+case UIImage(named: myImages2[0]):
+print("\(boos1.lifePoint)")
+case UIImage(named: myImages2[1]):
+print("\(boos2.lifePoint)")
+default:print("Error")
+}
+}
+return dice
+}
+}
+
     @IBAction func toHeroesPage(_ sender: Any)
     {
         
@@ -299,6 +480,7 @@ class ViewController: UIViewController
     
 @IBAction func textFieldfromLP(segue:UIStoryboardSegue)
 {
+    
 }
 }
 
--- Project02.1_JizanFantasy/Project02.1_JizanFantasy/ViewController2.swift
+++ Project02.1_JizanFantasy/Project02.1_JizanFantasy/ViewController2.swift
@@ -11,13 +11,17 @@ class ViewController2: UIViewController
 {
 var index: Int = 0
 var myImages = ["Wizard","Thief","Knight"]
-   
-    @IBOutlet var lpTextField: UITextField!
+    
+    @IBOutlet var lpLabel: UILabel!
     @IBOutlet var dfTextField: UITextField!
     @IBOutlet var pdTextField: UITextField!
     @IBOutlet var wdTextField: UITextField!
     @IBOutlet var scTextField: UITextField!
     
+//    var dfTextField = ""
+//    var pdTextField = ""
+//    var wdTextField = ""
+//    var scTextField = ""
     
     @IBOutlet var viewImage: UIImageView!
     @IBOutlet var heroNameLabel: UILabel!
@@ -27,13 +31,84 @@ var myImages = ["Wizard","Thief","Knight"]
  override func viewDidLoad()
  {
         super.viewDidLoad()
+//     let lpOfHero = ["250","170","250"]
+//
+//
+//     switch lpOfHero[index]
+//     {
+//     case :
+//
+//    lpLabel.text = "\(lpOfHero[1])"
+//     case :
+//    lpLabel.text = "\(lpOfHero[2])"
+//
+//     default:
+//         print("eroor")
+     
+   //  lpLabel.text = "\(lpOfHero)"
+     
+          
+//     if "\(String(describing: viewImage))" == myImages[0]
+//     {
+//         lpLabel.text = lpOfHero[0]
+//     }
+//     else if "\(String(describing: viewImage))" == myImages[1]
+//     {
+//         lpLabel.text = lpOfHero[1]
+//     }
+//     else if "\(String(describing: viewImage))" == myImages[2]
+//     {
+//         lpLabel.text = lpOfHero[2]
+//     }
+//
+     
+     
         viewImage.image = UIImage(named: myImages[0] )
         heroNameLabel.text = myImages[0]
+     
+     func swit()
+     {
+     let waz = 250
+     if heroNameLabel.text == myImages[0]
+     {
+     lpLabel.text = "\(waz)"
+     }
+     else if  heroNameLabel.text == myImages[1]
+     {
+     lpLabel.text = "\(waz)"
+     }
+ }
+     
+     func textfeild()
+     {
+         if myImages[index] == myImages[0]
+         {
+         dfTextField.text = "250"
+         }
+     }
  }
     
-
+//    func swit()
+//    {
+//    var myImages = ["Wizard","Thief","Knight"]
+//
+//    if heroNameLabel.text == UIImage(named: myImages2[0])
+//    {
+//    lpLabel.text = "\(250)"
+//    }
+//    else if  heroNameLabel == UIImage(named: myImages2[1])    {
+//    lpLabel.text = "\(250)"
+//    }
+//    else if heroNameLabel == UIImage(named: myImages2[2])
+//    {
+//    lpLabel.text = "\(250)"
+//    }
+//    }
+    
     @IBAction func gatewayToBattle(_ sender: Any)
     {
+        
+
 //        func send()
 //        {
 //
@@ -48,9 +123,10 @@ override func prepare(for segue: UIStoryboardSegue, sender: Any?)
 {
  let gatewayToBattle = segue.destination as! ViewController
         gatewayToBattle.heroImageinVC1.image = viewImage.image
-        
+    
+
  let gatewayToBattle1 = segue.destination as! ViewController
-        gatewayToBattle1.lpHeroLabel.text = lpTextField.text
+        gatewayToBattle1.lpHeroLabel.text = lpLabel.text
         
  let gatewayToBattle2 = segue.destination as! ViewController
         gatewayToBattle2.dfHeroLabel.text = dfTextField.text
@@ -64,9 +140,11 @@ override func prepare(for segue: UIStoryboardSegue, sender: Any?)
     let gatewayToBattle5 = segue.destination as! ViewController
     gatewayToBattle5.scHeroLabel.text = scTextField.text
 }
-    
+
     @IBAction func backHero(_ sender: Any)
     {
+//        swit()
+        
         if index > 0
         {
         index -= 1
@@ -77,8 +155,13 @@ override func prepare(for segue: UIStoryboardSegue, sender: Any?)
         }
         viewImage.image = UIImage(named: myImages[index] )
         heroNameLabel.text = myImages[index]
-    }
+        
+            
+        
+        
+        //lpLabel.text = myImages[0]
     
+    }
     @IBAction func nextHero(_ sender: Any)
     {
         if index < myImages.count - 1
@@ -93,6 +176,14 @@ override func prepare(for segue: UIStoryboardSegue, sender: Any?)
         heroNameLabel.text = myImages[index]
     }
     
+//    func heroSwitch()
+//    {
+//        if nextHero(Any) == myImages[0]
+//        {
+//
+//       }
+//    }
+
 }
 extension ViewController2: UITextFieldDelegate
 {
@@ -102,3 +193,4 @@ extension ViewController2: UITextFieldDelegate
         return true
     }
 }
+
