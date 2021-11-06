ca01155a2340d6887db7120602872bf55a6230a8
grand ahmad <grand23@grands-MacBook-Pro.local>
2021-11-07 00:14:14 +0300

    finally project

--- projectGamejazan/projectGamejazan/ViewController2.swift
+++ projectGamejazan/projectGamejazan/ViewController2.swift
@@ -8,86 +8,141 @@
 import Foundation
 import UIKit
 
-//desccribtion.LP.text = FieldLP.text
-
 class ViewController2: UIViewController{
     var name = "hello"
-    var arra = ["knight1","thief1","wiz100"]
-//    ربط الصوره
+    var heroesNames = ["knight","thief","wizard"]
+    var selectedHero: Boss!
+    var defense: Int = 0
+    var power: Int = 0
+    var weapon: Int = 0
+    //    ربط الصوره
+    @IBOutlet weak var pointsLabel: UILabel!
     @IBOutlet weak var createImage: UIImageView!
     //    اسم الجديد
     @IBOutlet weak var createName: UITextField!
-//    بيانات
+    //    بيانات
     @IBOutlet weak var createLPLabel: UITextField!
     @IBOutlet weak var createDFLabel: UITextField!
     @IBOutlet weak var createPDlabel: UITextField!
     @IBOutlet weak var createWDlabel: UITextField!
     @IBOutlet weak var createSClabel: UITextField!
-//    stoty
+    //    stoty
+    @IBOutlet weak var createButton: UIButton!
     @IBOutlet weak var storyPlayer: UILabel!
+    @IBOutlet weak var storyText: UITextView!
     
     override func viewDidLoad() {
-//        add()
         createName.delegate = self
         createLPLabel.delegate = self
         createDFLabel.delegate = self
         createPDlabel.delegate = self
-        createWDlabel .delegate = self
+        createWDlabel.delegate = self
         createSClabel.delegate = self
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard (_:)))
+        self.view.addGestureRecognizer(tapGesture)
     }
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let controlsSegue = segue.destination as? ViewController {
             controlsSegue.namePlayer2.text = createName.text
-            controlsSegue.playerLPLabel.text =  createLPLabel.text
-            controlsSegue.playerDFLabel.text = createDFLabel.text
-            controlsSegue.playerPDLabel.text = createPDlabel.text
-            controlsSegue.playerWDLabel.text = createWDlabel.text
+            selectedHero.defense = defense
+            selectedHero.powerDamage = power
+            selectedHero.weaponDamage = weapon
+            controlsSegue.playerLPLabel.text = createLPLabel.text
+            controlsSegue.playerDFLabel.text = "\(defense)"
+            controlsSegue.playerPDLabel.text = "\(power)"
+            controlsSegue.playerWDLabel.text = "\(weapon)"
             controlsSegue.playerSCLabel.text = createSClabel.text
             controlsSegue.playerImage.image = createImage.image
+            controlsSegue.rollDiceButton.isEnabled = true
+            controlsSegue.hero = selectedHero
+        }
+    }
+    func checkValues() -> Bool {
+        
+        if selectedHero == nil {
+            return false
+        }
+        if defense < 1 || defense > selectedHero.maxDefense {
+            return false
+        }
+        if power < 1 || power > selectedHero.maxPowerDamage {
+            return false
+        }
+        if weapon < 1 || weapon > selectedHero.maxWeaponDamage {
+            return false
+        }
+        
+        let total = selectedHero.lifePoint + selectedHero.specialCapacity + defense + power + weapon
+        if total > selectedHero.points {
+            return false
+        }
+        
+        return true
+    }
+    func loadHero() {
+        createDFLabel.isEnabled = true
+        createPDlabel.isEnabled = true
+        createWDlabel.isEnabled = true
+        createName.text = selectedHero.name
+        createImage.image = UIImage(named: selectedHero.imageName)
+        pointsLabel.text = "\(selectedHero.points)"
+        createLPLabel.text = "\(selectedHero.lifePoint)"
+        createSClabel.text = "\(selectedHero.specialCapacity)"
+    }
+    
+    @objc func dismissKeyboard (_ sender: UITapGestureRecognizer) {
+        createDFLabel.resignFirstResponder()
+        createPDlabel.resignFirstResponder()
+        createWDlabel.resignFirstResponder()
+        storyText.resignFirstResponder()
     }
 }
-}
-
 extension ViewController2: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
     }
-    func imageViewC() {
-        let images2: Int = .random(in: 0...arra.count - 1)
-        createImage.image = UIImage(named: arra[images2])
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        switch textField.tag{
+        case 1:
+            guard let heroName = textField.text else {
+                createButton.isEnabled = false
+                return
+            }
+            let trimmed = heroName.trimmingCharacters(in: .whitespacesAndNewlines).lowercased()
+            if heroesNames.contains(trimmed) {
+                switch trimmed {
+                case "knight": selectedHero = knight
+                case "thief": selectedHero = thief
+                case "wizard": selectedHero = wizard
+                default:
+                    print(0)
+                }
+                loadHero()
+            }
+        case 3:
+            if let dfText = textField.text {
+                defense = Int(dfText) ?? 0
+            }
+        case 4:
+            if let pdText = textField.text {
+                power = Int(pdText) ?? 0
+            }
+        case 5:
+            if let wdText = textField.text {
+                weapon = Int(wdText) ?? 0
+            }
+        default:
+            print(0)
+        }
+        if checkValues() {
+            createButton.isEnabled = true
+            print("Grand Ahmad")
+        }
     }
-//    func textFieldDidEndEditing(_ textField: UITextField) {
-//        let maxed: Int?
-//        if textField.tag == 3 {
-//        maxed: Int"\(createDFLabel.text)"
-//            }
-//        }
-    }
-//    func textFieldDidEndEditing(_ textField: UITextField) {
-//        print("0")
-//        if textField.tag == 3{
-//            let num = Int(textField.text)!
-//            
-//        }
-//
-//    }
-    
-//    func add() {
-//        let added: Int = .random(in: 1...3)
-//        switch added {
-//        case 1:
-//            createLPLabel.text = "\(knight.lifePoint)"
-//            createSClabel.text = "\(knight.specialCapcity)"
-//        case 2:
-//            createLPLabel.text = "\(wizard.lifePoint)"
-//            createSClabel.text = "\(wizard.specialCapcity)"
-//        case 3:
-//            createLPLabel.text = "\(thief.lifePoint)"
-//            createSClabel.text = "\(thief.specialCapcity)"
-//        default:
-//            print(0)
-//        }
-//    }
+}
+
 
