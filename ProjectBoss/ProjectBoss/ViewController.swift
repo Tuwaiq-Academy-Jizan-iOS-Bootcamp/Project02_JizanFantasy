6f2e39f7157bae031f13b836f1889eaeb893d679
YOUSEF ALMALKI <mmrdas600@gmail.com>
2021-11-09 21:34:18 +0300

    changed

--- ProjectBoss/ProjectBoss/ViewController.swift
+++ ProjectBoss/ProjectBoss/ViewController.swift
@@ -4,14 +4,223 @@
 //
 //  Created by يوسف جابر المالكي on 28/03/1443 AH.
 //
+
 import UIKit
-    
-    class ViewController: UIViewController {
-
-    
-        override func viewDidLoad() {
+struct Players {
+    var Name:String
+    var lifePoints:Int
+    var defense:Int
+    var powerDamage:Int
+    var weaponDamage:Int
+    var specialCapacity:Int
+    var specialCBonusPD:Int
+    var specialCBonusWD:Int
+    var specialCBonusLP:Int
+}
+class ViewController : UIViewController {
+    @IBOutlet weak var fightDes: UITextView!
+    @IBOutlet weak var diceLable: UILabel!
+    @IBOutlet weak var rollDiceButton: UIButton!
+    //Player Info... ((((SEGUE))))............
+    @IBOutlet weak var playerUImage: UIImageView!
+    @IBOutlet weak var playerName: UILabel!
+    @IBOutlet weak var playerLifePoint: UILabel!
+    @IBOutlet weak var playerDefenc: UILabel!
+    @IBOutlet weak var playerPoewrDamage: UILabel!
+    @IBOutlet weak var playerWeaponDamage: UILabel!
+    @IBOutlet weak var playerSpicalCabasity: UILabel!
+    var pLPoints = 0
+    var pDefense = 0
+    var pPDamage = 0
+    var pWDamage = 0
+    var pSCDamage = 0
+    var pSCBonusLP = 0
+    var pSCBonusPD = 0
+    var pSCBonusWD = 0
+    //Boss Info.........................
+    let randomBoss = Int.random(in: 1...2)
+    var boss1 = Players(Name: "First Boss Name", lifePoints: 250, defense: 30, powerDamage: 20, weaponDamage: 45, specialCapacity: 110, specialCBonusPD: 0, specialCBonusWD: 22, specialCBonusLP: 5)
+    var boss2 = Players(Name: "Second Boss Name", lifePoints: 170, defense: 25, powerDamage: 15, weaponDamage: 30, specialCapacity: 75, specialCBonusPD: 32, specialCBonusWD: 0, specialCBonusLP: 5)
+    var bossFName = ""
+    var bossFLP = 0
+    var bossFDF = 0
+    var bossFPD = 0
+    var bossFWD = 0
+    var bossFSC = 0
+    var bossFSCPB = 0
+    var bossFSCWB = 0
+    var bossFSCLPB = 0
+    @IBOutlet weak var bossImage: UIImageView!
+    @IBOutlet weak var bossName: UILabel!
+    @IBOutlet weak var bossLifePoint: UILabel!
+    @IBOutlet weak var bossDefenc: UILabel!
+    @IBOutlet weak var bossPowerDamage: UILabel!
+    @IBOutlet weak var bossWeaponDamge: UILabel!
+    @IBOutlet weak var bossSpichalCabasity: UILabel!
+    //To Show First...........
+    override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        rollDiceButton.isEnabled = false
+        if randomBoss == 1 {
+            bossImage.image = UIImage(named: "pass2")
+            bossName.text = boss1.Name
+            bossLifePoint.text = "\(boss1.lifePoints)"
+            bossDefenc.text = "\(boss1.defense)"
+            bossPowerDamage.text = "\(boss1.powerDamage)"
+            bossWeaponDamge.text = "\(boss1.weaponDamage)"
+            bossSpichalCabasity.text = "\(boss1.specialCapacity)Damage / +\(boss1.specialCBonusWD)WD / +\(boss1.specialCBonusPD)PD / +\(boss1.specialCBonusLP)LP"
+            bossFName = boss1.Name
+            bossFLP = boss1.lifePoints
+            bossFDF = boss1.defense
+            bossFPD = boss1.powerDamage
+            bossFWD = boss1.weaponDamage
+            bossFSC = boss1.specialCapacity
+            bossFSCPB = boss1.specialCBonusPD
+            bossFSCWB = boss1.specialCBonusWD
+            bossFSCLPB = boss1.specialCBonusLP
+        }else {
+            bossImage.image = UIImage(named: "knight")
+            bossName.text = boss2.Name
+            bossLifePoint.text = "\(boss2.lifePoints)"
+            bossDefenc.text = "\(boss2.defense)"
+            bossPowerDamage.text = "\(boss2.powerDamage)"
+            bossWeaponDamge.text = "\(boss2.weaponDamage)"
+            bossSpichalCabasity.text = "\(boss2.specialCapacity)Damage / +\(boss2.specialCBonusWD)WD / +\(boss2.specialCBonusPD)PD / +\(boss2.specialCBonusLP)LP"
+            bossFName = boss2.Name
+            bossFLP = boss2.lifePoints
+            bossFDF = boss2.defense
+            bossFPD = boss2.powerDamage
+            bossFWD = boss2.weaponDamage
+            bossFSC = boss2.specialCapacity
+            bossFSCPB = boss2.specialCBonusPD
+            bossFSCWB = boss2.specialCBonusWD
+            bossFSCLPB = boss2.specialCBonusLP
+        }
     }
+    var turn = true
+    @IBAction func rollTheDise2(_ sender: Any) {
+        let rollDice = Int.random(in: 1...20)
+        diceLable.text = "Dice Roll is : \(rollDice)"
+        if rollDice >= 1 && rollDice <= 9 && turn == true {
+            turn = false
+            var damageOnBoss = pPDamage - bossFDF
+            if damageOnBoss < 0 {
+                bossFLP -= 0
+            }else {
+                bossFLP -= damageOnBoss
+                deadLine()
+                bossLifePoint.text = "\(bossFLP)"
+            }
+            if damageOnBoss < 0 {
+                damageOnBoss = 0
+            }
+            fightDes.text += "  \(playerName.text!) use his power to damage \(bossName.text!) by \(damageOnBoss)"
+            print("Player")
+        }else if rollDice >= 10 && rollDice <= 19 && turn == true {
+            turn = false
+            var damageOnBoss = pWDamage - bossFDF
+            if damageOnBoss < 0 {
+                bossFLP -= 0
+            }else {
+                bossFLP -= damageOnBoss
+                deadLine()
+                bossLifePoint.text = "\(bossFLP)"
+            }
+            if damageOnBoss < 0 {
+                damageOnBoss = 0
+            }
+            fightDes.text += "  \(playerName.text!) use his weapon to damage \(bossName.text!) by \(damageOnBoss)"
+            print("Player")
+        }else if rollDice == 20 && turn == true {
+            turn = false
+            var damageOnBoss = pSCDamage - bossFDF
+            if damageOnBoss < 0 {
+                bossFLP -= 0
+            }else {
+                bossFLP -= damageOnBoss
+                deadLine()
+                bossLifePoint.text = "\(bossFLP)"
+            }
+            if damageOnBoss < 0 {
+                damageOnBoss = 0
+            }
+            pLPoints += pSCBonusLP
+            pPDamage += pSCBonusPD
+            pWDamage += pSCBonusWD
+            playerLifePoint.text = "\(pLPoints)"
+            playerPoewrDamage.text = "\(pPDamage)"
+            playerWeaponDamage.text = "\(pWDamage)"
+            fightDes.text += "  \(playerName.text!) use his Special Capacity to damage \(bossName.text!) by \(damageOnBoss) and recover \(pSCBonusLP)LP and increase his weapon Damage by \(pSCBonusWD)WD also his power damage by \(pSCBonusPD)PD"
+            print("Player")
+        }else if rollDice >= 1 && rollDice <= 9 && turn == false {
+            turn = true
+            var damageOnPlayer = bossFPD - pDefense
+            if damageOnPlayer < 0 {
+                pLPoints -= 0
+            }else {
+                pLPoints -= damageOnPlayer
+                deadLine()
+                playerLifePoint.text = "\(pLPoints)"
+            }
+            if damageOnPlayer < 0 {
+                damageOnPlayer = 0
+            }
+            fightDes.text += "  \(bossName.text!) use his power to damage \(playerName.text!) by \(damageOnPlayer)"
+            print("Boss")
+        }else if rollDice >= 10 && rollDice <= 19 && turn == false {
+            turn = true
+            var damageOnPlayer = bossFWD - pDefense
+            if damageOnPlayer < 0 {
+                pLPoints -= 0
+            }else {
+                pLPoints -= damageOnPlayer
+                deadLine()
+                playerLifePoint.text = "\(pLPoints)"
+            }
+            if damageOnPlayer < 0 {
+                damageOnPlayer = 0
+            }
+            fightDes.text += "  \(bossName.text!) use his weapon to damage \(playerName.text!) by \(damageOnPlayer)"
+            print("Boss")
+        }else if rollDice == 20 && turn == false {
+            turn = true
+            var damageOnPlayer = bossFSC - pDefense
+            if damageOnPlayer < 0 {
+                pLPoints -= 0
+            }else {
+                pLPoints -= damageOnPlayer
+                deadLine()
+                playerLifePoint.text = "\(pLPoints)"
+            }
+            if damageOnPlayer < 0 {
+                damageOnPlayer = 0
+            }
+            bossFLP += bossFSCLPB
+            bossFLP += bossFSCLPB
+            bossFWD += bossFSCWB
+            bossLifePoint.text = "\(bossFLP)"
+            bossPowerDamage.text = "\(bossFPD)"
+            bossWeaponDamge.text = "\(bossFWD)"
+            fightDes.text += "  \(bossName.text!) use his Special Capacity to damage \( playerName.text!) by \(damageOnPlayer) and recover \(boss1.specialCBonusLP)LP and increase his weapon Damage by \(boss1.specialCBonusWD)WD also his power damage by \(boss1.specialCBonusPD)PD"
+            print("Boss")
+        }
+    }
+    func deadLine() {
+        if pLPoints <= 0 {
+            pLPoints = 0
+            rollDiceButton.isEnabled = false
+            diceLable.text = "YOU LOST.."
+        }else if bossFLP <= 0 {
+            bossFLP = 0
+            rollDiceButton.isEnabled = false
+            diceLable.text = "YOU WIN !"
+        }
+    }
+    @IBAction func choosePlayer(_ sender: Any) {
+        rollDiceButton.isEnabled = true
+    }
+    //Unwine....
+    @IBAction func getBack(segue:UIStoryboardSegue){
+    }
+}
 
-    }
--- ProjectBoss/ProjectBoss/ViewControllerTow.swift
+++ ProjectBoss/ProjectBoss/ViewControllerTow.swift
@@ -6,22 +6,251 @@
 //
 
 import UIKit
-
 class ViewControllerTow: UIViewController {
-
+    @IBOutlet weak var save1: UIButton!
+    @IBOutlet weak var goToFight: UIButton!
+    @IBOutlet weak var selectPlayer: UIPickerView!
+    let classeArray = ["knight","Wizard","thief"]
+    @IBOutlet weak var playerName1: UITextField!
+    @IBOutlet weak var playerImage1: UIImageView!
+    var pImage = 0
+    @IBOutlet weak var point1: UILabel!
+    var playerPoint = 0
+    var playerMaxDefance = 0
+    var playerMaxPowerDamage = 0
+    var playerMaxWD = 0
+    var playerMaxPoints = 0
+    @IBOutlet weak var maxpoints: UILabel!
+    @IBOutlet weak var playerLifePoints: UITextField!
+    @IBOutlet weak var playerDefence: UITextField!
+    @IBOutlet weak var playerPowerDamge: UITextField!
+    @IBOutlet weak var playerWeaponDamage: UITextField!
+    var lpPlayer = 0
+    var dFPlayer = 0
+    var pDPlayer = 0
+    var wDPlayer = 0
+    var playerSC = 0
+    var playerSCBonusPD = 0
+    var playerSCBonusWD = 0
+    var playerSCBonusLP = 0
+    var pSCFightComment = ""
+    @IBOutlet weak var specialCapacity1: UILabel!
+    //Override
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        selectPlayer.delegate = self
+        selectPlayer.dataSource = self
+        let tap:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(closeKeyboard))
+        self.view.addGestureRecognizer(tap)
+        knight()
+        goToFight.isEnabled = false
+        save1.isEnabled = false
     }
-    
-
-   
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    @IBAction func lifePoints(_ sender: Any) {
+        if let state = Int(playerLifePoints.text!) {
+            if state > playerPoint {
+                notEnoughPoints()
+            }else{
+                lpPlayer = state
+                playerPoint -= state
+                point1.text = "\(playerPoint)"
+                playerLifePoints.isEnabled = false
+                save1.isEnabled = true
+            }
+        }
+    }
+    @IBAction func deffense(_ sender: Any) {
+        if let state = Int(playerDefence.text!) {
+            if state > playerPoint {
+                notEnoughPoints()
+            }else if state > playerMaxDefance {
+                aboveTheMax()
+            }else{
+                dFPlayer = state
+                playerPoint -= state
+                point1.text = "\(playerPoint)"
+                playerDefence.isEnabled = false
+                save1.isEnabled = true
+            }
+        }
+    }
+    @IBAction func powerDamage(_ sender: Any) {
+        if let state = Int(playerPowerDamge.text!) {
+            if state > playerPoint {
+                notEnoughPoints()
+            }else if state > playerMaxPowerDamage {
+                aboveTheMax()
+            }else{
+                pDPlayer = state
+                playerPoint -= state
+                point1.text = "\(playerPoint)"
+                playerPowerDamge.isEnabled = false
+                save1.isEnabled = true
+            }
+        }
+    }
+    @IBAction func weaponDamage(_ sender: Any) {
+        if let state = Int(playerWeaponDamage.text!) {
+            if state > playerPoint {
+                notEnoughPoints()
+            }else if state > playerMaxWD {
+                aboveTheMax()
+            }else{
+                wDPlayer = state
+                playerPoint -= state
+                point1.text = "\(playerPoint)"
+                playerWeaponDamage.isEnabled = false
+                save1.isEnabled = true
+            }
+        }
+    }
+    @IBAction func Reset(_ sender: Any) {
+        resetState()
+        save1.isEnabled = false
+    }
+    @IBAction func saveTheState(_ sender: Any) {
+        if let state = Int(playerLifePoints.text!), let state2 = Int(playerDefence.text!), let state3 = Int(playerPowerDamge.text!), let state4 = Int(playerWeaponDamage.text!) {
+            let maxTextField = state + state2 + state3 + state4
+            let stateThatSend = lpPlayer + dFPlayer + pDPlayer + wDPlayer
+            if maxTextField == playerMaxPoints && stateThatSend == playerMaxPoints {
+                goToFight.isEnabled = true
+            }else {
+                youCantGo()
+            }
+        }
+    }
+    //Player Info........ (((SEGUE)))//
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-
+        let playerData = segue.destination as? ViewController
+        playerData?.playerName.text = playerName1.text
+        playerData?.pLPoints = lpPlayer
+        playerData?.pDefense = dFPlayer
+        playerData?.pPDamage = pDPlayer
+        playerData?.pWDamage = wDPlayer
+        playerData?.pSCDamage = playerSC
+        playerData?.pSCBonusLP = playerSCBonusLP
+        playerData?.pSCBonusPD = playerSCBonusPD
+        playerData?.pSCBonusWD = playerSCBonusWD
+        playerData?.playerSpicalCabasity.text = pSCFightComment
+        playerData?.playerUImage.image = playerImage1.image
+        playerData?.playerLifePoint.text = playerLifePoints.text
+        playerData?.playerDefenc.text = playerDefence.text
+        playerData?.playerPoewrDamage.text = playerPowerDamge.text
+        playerData?.playerWeaponDamage.text = playerWeaponDamage.text
     }
-   
-
+    //Fonction To Close The Daim Keyboard
+    @objc func closeKeyboard() {
+        self.view.endEditing(true)
+    }
+    func notEnoughPoints() {
+        let alert = UIAlertController(title: "Not Enough Points", message: "", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in}))
+        present(alert, animated: true)
+    }
+    func aboveTheMax() {
+        let alert = UIAlertController(title: "Sorry Above The Max", message: "", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in}))
+        present(alert, animated: true)
+    }
+    func youCantGo() {
+        let alert = UIAlertController(title: "Please Check Your Points", message: "", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in}))
+        present(alert, animated: true)
+    }
+    func resetState() {
+        if pImage == 1 {
+            knight()
+        }else if pImage == 2 {
+            wizard()
+        }else {
+            thief()
+        }
+    }
+    func knight() {
+        playerLifePoints.isEnabled = true
+        playerWeaponDamage.isEnabled = true
+        playerPowerDamge.isEnabled = true
+        playerDefence.isEnabled = true
+        playerImage1.image = UIImage(named: "knight")
+        pImage = 1
+        playerMaxPoints = 150
+        playerPoint = 150
+        playerMaxDefance = 20
+        playerMaxPowerDamage = 30
+        playerMaxWD = 40
+        playerSC = 100
+        playerSCBonusPD = 0
+        playerSCBonusWD = 10
+        playerSCBonusLP = 5
+        maxpoints.text = "Max(DF)20 Max(PD)30 Max(WD)40"
+        point1.text = "\(playerPoint)"
+        pSCFightComment = "\(playerSCBonusPD)PD / \(playerSCBonusWD)WD / \(playerSCBonusLP)LP"
+        specialCapacity1.text = "When Knight use his SC he will delever a massive 100 Damage to his opponent and recover a small amont of life points +5LP and increas his weapon damage by +10WD"
+        
+    }
+    func wizard() {
+        playerLifePoints.isEnabled = true
+        playerWeaponDamage.isEnabled = true
+        playerPowerDamge.isEnabled = true
+        playerDefence.isEnabled = true
+        playerImage1.image = UIImage(named: "Wizard")
+        pImage = 2
+        playerMaxPoints = 195
+        playerPoint = 195
+        playerMaxDefance = 15
+        playerMaxPowerDamage = 70
+        playerMaxWD = 20
+        playerSC = 50
+        playerSCBonusPD = 10
+        playerSCBonusWD = 0
+        playerSCBonusLP = 35
+        point1.text = "\(playerPoint)"
+        maxpoints.text = "Max(DF)15 Max(PD)70 Max(WD)20"
+        pSCFightComment = "\(playerSCBonusPD)PD / \(playerSCBonusWD)WD / \(playerSCBonusLP)LP"
+        specialCapacity1.text = "When Wizard use his SC he will do a 50 Damage to his opponent and recover a larg amont of life points +35LP and increas his power damage by +10PD"
+        
+    }
+    func thief() {
+        playerLifePoints.isEnabled = true
+        playerWeaponDamage.isEnabled = true
+        playerPowerDamge.isEnabled = true
+        playerDefence.isEnabled = true
+        playerImage1.image = UIImage(named: "thief")
+        pImage = 3
+        playerMaxPoints = 135
+        playerPoint = 135
+        playerMaxDefance = 25
+        playerMaxPowerDamage = 15
+        playerMaxWD = 30
+        playerSC = 75
+        playerSCBonusPD = 0
+        playerSCBonusWD = 35
+        playerSCBonusLP = 5
+        point1.text = "\(playerPoint)"
+        maxpoints.text = "Max(DF)25 Max(PD)15 Max(WD)30"
+        pSCFightComment = "\(playerSCBonusPD)PD / \(playerSCBonusWD)WD / \(playerSCBonusLP)LP"
+        specialCapacity1.text = "When Thief use his SC he will delever a 75 Damage to his opponent and recover a small of life points +5LP and greatly increas his power damage by +35PD"
+    }
 }
+extension ViewControllerTow: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return classeArray.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return classeArray[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if selectPlayer.selectedRow(inComponent: 0) == 0 {
+            knight()
+        }else if selectPlayer.selectedRow(inComponent: 0) == 1 {
+            wizard()
+        }else {
+            thief()
+        }
+    }
+}
+//end...........
+
